1ddd21686372f9bd7a2f0f467ba81de8ebdae61c
QuentinMerabet <quentin.merabet@yahoo.com>
2022-06-06 14:41:28 +0200

    last block clone fix

--- components/FormAddress.js
+++ components/FormAddress.js
@@ -13,7 +13,6 @@ export default function FormAddress(props) {
   const [totalResult, setTotalResult] = useState();
 
   const id = useId();
-  const apiKey = "X2BBEZYNQQCBH7QM1PZ69EFRSGUE3SQBZ6";
   const maxApiResult = 10_000;
 
   const KgCo2PerGasOld = 0.0001809589427;
@@ -22,27 +21,58 @@ export default function FormAddress(props) {
 
   async function submitAddress(event) {
     setIsFetching(true);
+    let startingTime = Date.now();
     event.preventDefault(); // Prevent refresh
 
+    // Init
     let txPages = []; // Storing results in pages in case their is more than maxApiResult
     let txList = {}; // Current tx list returned by API
     let totalTxCount = 0; // Total number of tx received
-
-    // Fetching Etherscan
-    let apiResult = await fetchJSON(buildApiUrl(address), 99999999);
-    txList = apiResult.result;
-    txPages.push(txList);
-
     let lowestBlock;
-    while (txList.length == maxApiResult) {
-      lowestBlock = txList[txList.length - 1].blockNumber;
-      let currentPage = txPages.length + 1;
-      console.log("there's more! Fetching page", currentPage);
-      apiResult = await fetchJSON(buildApiUrl(address, lowestBlock));
+    let done = false;
 
-      txList = apiResult.result;
-      console.log("taille dernier retour:", txList.length);
-      console.log("dernier element:", txList[txList.length - 1].gasUsed);
+    // Fetching Etherscan API loop
+    while (!done) {
+      // Keeping track of the request id
+      let currentPage = txPages.length + 1;
+      console.log(
+        "Request number",
+        currentPage,
+        "TxList length",
+        txList.length
+      );
+      // Fetching
+      let apiResult = await fetchJSON(
+        buildApiUrl("txlist", address, lowestBlock)
+      );
+      // Error handling
+      if (
+        (apiResult.status === "0" &&
+          apiResult.message === "No transactions found") ||
+        apiResult.status !== "1"
+      ) {
+        console.log(apiResult.message);
+        setError(apiResult.message);
+        setIsFetching(false);
+        done = true;
+        return;
+      }
+
+      // If not the last page/request
+      if (apiResult.result.length == maxApiResult) {
+        done = false;
+        // Updating the lowest block fetched based on the direct API result
+        lowestBlock = apiResult.result[apiResult.result.length - 1].blockNumber;
+        // Filter out the last block received as we don't know if its complete (no more cloned tx fix)
+        txList = apiResult.result.filter(
+          (txs) => txs.blockNumber > lowestBlock
+        );
+      } else {
+        // Not filtering results as it's the last page and we have the garanty to have every data fitting in the request's response.
+        txList = apiResult.result;
+        done = true;
+      }
+      // Storing the transactions list in an array (one row per request to the API)
       txPages.push(txList);
     }
 
@@ -54,18 +84,28 @@ export default function FormAddress(props) {
         totalTxCount++;
       }
     }
+
+    // Printing the data
     setCalculation(
-      `Pages: ${
+      `Total API Request: ${
         txPages.length
-      } / Total TX Count: ${totalTxCount} / Total gas used: ${totalGasUsed} Gwei / Total KgCO2: ${Math.round(
+      } / Total TX Count: ${totalTxCount} / Total gas used: ${totalGasUsed} Gwei / Total KgCO2 (Carbon.fyi algo): ${Math.round(
         totalGasUsed * KgCo2PerGasOld
-      )} / Total KgCO2 New: ${Math.round(
+      )} / Total KgCO2 (C-Level algo): ${Math.round(
         totalGasUsed * KgCo2PerGasNew
-      )} / Total KWH: ${Math.round(totalGasUsed * KwhPerGasNew)}`
+      )} / Total KWH (C-Level algo): ${Math.round(totalGasUsed * KwhPerGasNew)}`
+    );
+    // Execution time for science!
+    let endingTime = Date.now();
+    console.log(
+      "Execution time:",
+      Math.round((endingTime - startingTime) / 1000),
+      "seconds"
     );
     setIsFetching(false);
   }
 
+  // Render component
   if (isFetching) {
     return (
       <>
--- utils/buildApiUrl.js
+++ utils/buildApiUrl.js
@@ -1,6 +1,11 @@
-export function buildApiUrl(address, endBlock) {
+export function buildApiUrl(request, address, endBlock = 99999999) {
   const apiKey = "X2BBEZYNQQCBH7QM1PZ69EFRSGUE3SQBZ6";
 
-  const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=${endBlock}&sort=desc&apikey=${apiKey}`;
-  return url;
+  switch (request) {
+    case "txlist":
+      return `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=${endBlock}&sort=desc&apikey=${apiKey}`;
+
+    case "totalsupply":
+      return `https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${address}&apikey=${apiKey}`;
+  }
 }
