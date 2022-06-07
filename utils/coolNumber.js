65b8454e846d4353f71a7cb62cc3bfb994cfa8b3
QuentinMerabet <quentin.merabet@yahoo.com>
2022-06-07 06:15:17 +0200

    billions support for numbers

--- utils/coolNumber.js
+++ utils/coolNumber.js
@@ -3,7 +3,9 @@ export function coolNumber(n) {
     return Math.round(n);
   } else if (n < 1_000_000) {
     return Math.round(n / 1_000) + "K";
-  } else {
+  } else if (n < 1_000_000_000) {
     return Math.round(n / 1_000_000) + "M";
+  } else {
+    return Math.round(n / 1_000_000_000) + "B";
   }
 }
